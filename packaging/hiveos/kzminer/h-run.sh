44c4c677a778368c448f5348b0a8442f9fa6e393
Koriaz98 <170395327+Koriaz98@users.noreply.github.com>
2026-07-17 23:29:55 +0000

    fix: HiveOS h-run.sh uses a version-less binary name (kzminer) so future version bumps don't require touching this script

--- packaging/hiveos/kzminer/h-run.sh
+++ packaging/hiveos/kzminer/h-run.sh
@@ -3,6 +3,10 @@
 # %WORKER_NAME% deja substitues par Hive - mais PAS %URL% (confirme
 # sur un vrai systeme le 17/07/2026). On le remplace nous-memes avec
 # $CUSTOM_URL, qui contient la vraie valeur.
+#
+# Le binaire est nomme "kzminer" (sans suffixe de version) a
+# l'interieur de ce paquet specifiquement pour HiveOS, pour ne jamais
+# avoir a modifier ce script lors des futures mises a jour de version.
 
 . /hive-config/wallet.conf
 . ./h-manifest.conf
@@ -12,4 +16,4 @@ mkdir -p "$(dirname "$log_file")"
 
 final_config="${CUSTOM_USER_CONFIG//%URL%/$CUSTOM_URL}"
 
-exec ./kzminer-v0.9.7 $final_config >>"$log_file" 2>&1
+exec ./kzminer $final_config >>"$log_file" 2>&1
